48549f26ec0d94d09856b4e91b393ce4520321c7
Ben <1622998tud.hs-mannheim.de>
2017-11-22 13:27:27 +0100

    finish!

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -15,9 +15,26 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class Proxy
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = {}
+    @messages.default = 0
   end
 
+  def messages
+    @messages.keys
+  end
+
+  def called?(method_name)
+    @messages.keys.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages[method_name]
+  end
+
+  def method_missing(method_name, *args, &block)
+    @messages[method_name] += 1
+    @object.send(method_name, *args, &block)
+  end
   # WRITE CODE HERE
 end
 
