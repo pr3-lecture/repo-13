decf562c43ddb202e9ff5fbaa3b34ce46e1b4670
Ben <1622998tud.hs-mannheim.de>
2017-11-22 11:06:38 +0100

    task 220

--- koans/about_inheritance.rb
+++ koans/about_inheritance.rb
@@ -40,7 +40,7 @@ class AboutInheritance < Neo::Koan
     chico = Chihuahua.new("Chico")
     assert_equal :happy, chico.wag
 
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       fido = Dog.new("Fido")
       fido.wag
     end
@@ -51,7 +51,7 @@ class AboutInheritance < Neo::Koan
     assert_equal __, chico.bark
 
     fido = Dog.new("Fido")
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
   # ------------------------------------------------------------------
@@ -64,7 +64,7 @@ class AboutInheritance < Neo::Koan
 
   def test_subclasses_can_invoke_parent_behavior_via_super
     ralph = BullDog.new("Ralph")
-    assert_equal __, ralph.bark
+    assert_equal "WOOF, GROWL", ralph.bark
   end
 
   # ------------------------------------------------------------------
